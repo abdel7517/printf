6347b972ef5e7dd0d038c68032ea6752f63e9b5c
Abdelkarim CHABANE <abchaban@e1r11p5.clusters.42paris.fr>
2022-11-17 18:48:48 +0100

    error norme *.h file

--- ft_printf.h
+++ ft_printf.h
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ftprintf.h                                         :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abchaban <abchaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 11:05:39 by abchaban          #+#    #+#             */
-/*   Updated: 2022/11/17 14:06:03 by abchaban         ###   ########.fr       */
+/*   Created: 2022/11/17 18:45:12 by abchaban          #+#    #+#             */
+/*   Updated: 2022/11/17 18:46:47 by abchaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FTPRINTF_H
-# define FTPRINTF_H
-# include <stdarg.h>
-# include <stdio.h>
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdint.h>
+# include <stdarg.h>
 
+int		ft_printf(const char *s, ...);
+int		ft_putnbr_base(int nbr, char *base);
+int		ft_putnbr_u(unsigned int n);
+int		ft_putstr(char *str);
+int		ft_putchar(char c);
+int		ft_putnbr(int n);
 
-int	ft_printf(const char*, ...);
-int	ft_putnbr_base(int nbr, char *base);
-int	ft_putnbr_u(unsigned int n);
-int	ft_putstr(char *str);
-int	ft_putchar(char c);
-int	ft_putnbr(int n);
 #endif
